8d2c21d4c42c32527dc81f2f345494d4a7eae626
Dhruv <dhruv1592001@gmail.com>
2021-06-27 15:11:59 +0530

    adding Unity Project

---
+++ Assets/VBTN.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VBTN : MonoBehaviour
+{
+    public GameObject echoObject;
+    public GameObject PlaneObject;
+    public GameObject Scriptvbtn2;
+    public VirtualButtonBehaviour Vb; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vb.RegisterOnButtonPressed(OnButtonPressed);
+        echoObject.SetActive(false);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        echoObject.SetActive(true);
+        PlaneObject.SetActive(false);
+        Scriptvbtn2.SetActive(false);  
+    }
+}
---
+++ Assets/VBTNSec.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VBTNSec : MonoBehaviour
+{
+
+	public GameObject Object;
+	public GameObject Plane1Object;
+	public GameObject Button1Object;
+	public VirtualButtonBehaviour Vb1;
+    // Start is called before the first frame update
+    void Start()
+    {
+	Vb1.RegisterOnButtonPressed(OnButtonPressesed1);
+	Object.SetActive(false);
+        
+    }
+
+    public void OnButtonPressesed1(VirtualButtonBehaviour vb1)
+	{
+		Object.SetActive(true);
+		Plane1Object.SetActive(false);
+		Button1Object.SetActive(false);
+	}
+}
